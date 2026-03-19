8dfbed55c8758089557377ac91156df3ad6ff113
AyanXDeveloper <ayyanzaveri@gmail.com>
2026-03-20 00:00:21 +0500

    Dynamic Counter Application

---
+++ src/components/CounterCard.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const CounterCard = React.memo(() => {
+
+    const [count, setCount] = useState(0)
+    const [step, setStep] = useState(1)
+
+    return (
+        <>
+            <div className="w-110 h-120 bg-[#2D2D2D] rounded-md shadow-2xl p-5 inter">
+                <div className="w-full h-1/10 ">
+                    <h2 className="text-white text-3xl font-bold">Counter App</h2>
+                </div>
+                <div className="flex justify-center items-center h-4/10 ">
+                    <span className={`text-[#39E079] text-[120px] font-bold`}>{count}</span>
+                </div>
+                <div className='flex flex-col gap-3 h-5/10 py-2'>
+                    <div className='w-full h-3/7 text-white text-center flex flex-col gap-2'>
+                        <label className='text-[#CCCCCC] text-sm'>Step Value</label>
+                        <input className='w-full  bg-[#1A1A1A] border border-[#555555] text-center rounded-lg items-center h-full' type="number" value={step} onChange={(e) => setStep(Number(e.target.value))} />
+                    </div>
+                    <div className='flex justify-between gap-5 w-full h-2/7'>
+                        <button onClick={() => setCount(count + step)} className='w-1/2 bg-[#39E079] h-full text-white rounded-lg text-[18px] font-bold transition-all duration-300 hover:-translate-y-1 hover:shadow-[0_4px_12px_rgba(57,224,121,0.3)] cursor-pointer'>Increase</button>
+                        <button onClick={() => setCount(count - step)} className='w-1/2 bg-[#1A4A2A] h-full rounded-lg text-[18px] font-bold text-[#39E079] transition-all duration-300 ease-out hover:-translate-y-1 hover:shadow-[0px_4px_20px_#4f735c] cursor-pointer'>Decrease</button>
+                    </div>
+                    <button onClick={() => {
+                        setCount(0);
+                        setStep(1);
+                    }} className='bg-[#555555] w-full h-2/7 text-white rounded-lg text-[18px] font-bold transition-all duration-300 hover:bg-[#666666] cursor-pointer'>Reset</button>
+                </div>
+            </div>
+        </>
+    )
+})
+
+export default CounterCard
